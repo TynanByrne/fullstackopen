f2a1ef070a67c5c2c9d171b4f11febe413d8b15a
Tynan Byrne <tsbyrne6@gmail.com>
2020-10-21 12:04:31 +0100

    Added authentication with JWT

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', (req, res) => {
   Blog
@@ -12,10 +22,21 @@ blogsRouter.get('/', (req, res) => {
     .catch(error => logger.error(error))
 })
 
-blogsRouter.post('/', (request, response) => {
-  let id = "5f8f7793d51307089432047e"
-  const blog = new Blog({ ...request.body,
-    user: id })
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
 
   if (!blog.title || !blog.url) {
     return response.status(400).json({ error: 'Title or URL of the blog missing' })
@@ -23,13 +44,11 @@ blogsRouter.post('/', (request, response) => {
   if (!blog.likes) {
     blog.likes = 0
   }
-  
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => console.error(error))
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', (req, res) => {
---
+++ part4/bloglist/utils/tokenExtractor.js
@@ -0,0 +1,1 @@
+const tokenExtractor = request => 
