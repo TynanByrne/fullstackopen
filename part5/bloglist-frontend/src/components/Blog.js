f905a5f703e896d77036aeebccbc0bb87504dd57
Tynan Byrne <tsbyrne6@gmail.com>
2020-11-06 23:35:03 +0000

    Added some styling

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -2,42 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 /* import PropTypes from 'prop-types' */
 const Blog = ({ blog, user, handleDelete, handleUpdate }) => {
-  /* const [visible, setVisible] = useState(false)
-
-  const toggleVisiblity = () => {
-    setVisible(!visible)
-  } */
-
-  
-
-  
-
   /* Blog.propTypes = {
     blog: PropTypes.object.isRequired,
     user: PropTypes.object.isRequired,
     deleteBlog: PropTypes.func.isRequired,
     updateBlog: PropTypes.func.isRequired
   } */
-
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
   return (
-    /* (visible) ?
-      <div style={blogStyle} className='blogDetailed'>
-        {blog.title} {blog.author} <button onClick={toggleVisiblity}>hide</button>
-        <div>
-          <p>{blog.url}</p>
-          <p>likes <span id='likes'>{blog.likes}</span><button id='like' onClick={handleLike}>like</button></p>
-          <p> added by {blog.user.name}</p>
-          {allowDelete()}
-        </div>
-      </div> : */
-      <div style={blogStyle} className='listedBlog'>
+      <div className='listedBlog'>
         <Link to={`/blogs/${blog.id}`} >{blog.title} by {blog.author}</Link>
       </div>
   )
--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -1,15 +1,26 @@
+import { Table, TableBody, TableContainer, TableRow, TableCell } from '@material-ui/core'
 import React from 'react'
 import Blog from './Blog'
 
 const BlogList = ({ blogs, compare, handleDelete, handleUpdate, user }) => {
 
   return (
-    <>
-      {blogs.sort(compare).map(blog =>
-        <Blog key={blog.id} blog={blog} user={user} handleDelete={handleDelete}
-          handleUpdate={handleUpdate} />
-      )}
-    </>
+    <TableContainer>
+      <Table>
+        <TableBody>
+          {blogs.sort(compare).map(blog =>
+            <TableRow key={blog.id}>
+              <TableCell>
+                <Blog key={blog.id} blog={blog} user={user} handleDelete={handleDelete}
+                  handleUpdate={handleUpdate} />
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+
+      </Table>
+
+    </TableContainer>
   )
 }
 
